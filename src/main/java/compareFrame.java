b40edcdbec88cd0c0426158803849b7b5581a1ee
nast97 <nastoukos@gmail.com>
2020-05-26 21:04:38 +0300

    demo

--- src/main/java/compareFrame.java
+++ src/main/java/compareFrame.java
@@ -3,4 +3,12 @@ import javax.swing.*;
 public class compareFrame extends JFrame {
     private JComboBox comboBox1;
     private JComboBox comboBox2;
+    private JRadioButton radioButton1;
+    private JRadioButton radioButton2;
+    private JRadioButton radioButton3;
+    private JRadioButton radioButton4;
+    private JComboBox comboBox3;
+    private JComboBox comboBox4;
+    private JTextField dateTextField1;
+    private JTextField dateTextField2;
 }
