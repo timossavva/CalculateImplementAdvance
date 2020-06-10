4cc34dc18028d23995777e2a1777b5292c151a49
Timos Savva <timotheos.savva12@gmail.com>
2020-06-10 22:39:01 +0300

    added store CRUD

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,17 @@
+import branches.objects.SaleThread;
 import users.ui.LoginForm;
 
 import javax.swing.*;
+import java.util.Calendar;
 
 public class Main {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(LoginForm::new);
+
+        Calendar stopTime = Calendar.getInstance();
+        stopTime.set(Calendar.HOUR, 11);
+        SaleThread salethread = new SaleThread(1, stopTime);
     }
 
 }
--- src/main/java/branches/objects/SaleThread.java
+++ src/main/java/branches/objects/SaleThread.java
@@ -1,13 +1,16 @@
 package branches.objects;
+
 import products.objects.BranchProduct;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Random;
 
 public class SaleThread implements Runnable {
-    private int delay;
-    private Calendar stopTime;
+    private final int delay;
+    private final Calendar stopTime;
 
-    SaleThread(int delay, Calendar stopTime) {
+    public SaleThread(int delay, Calendar stopTime) {
         this.delay = delay;
         this.stopTime = stopTime;
 
@@ -19,7 +22,7 @@ public class SaleThread implements Runnable {
     public void run() {
         try {
             boolean stopSales = false;
-            while(!stopSales) {
+            while (!stopSales) {
                 if (Thread.interrupted()) {
                     throw new InterruptedException();
                 }
@@ -31,30 +34,27 @@ public class SaleThread implements Runnable {
                 }
 
                 ArrayList<Branch> branchList = BranchList.getBranchList();
-                double sum = 0;
                 for (Branch b : branchList) {
                     Random rand = new Random(System.currentTimeMillis());
-                    BranchProduct[] ArrayOfProducts = (BranchProduct[]) b.getBranchProducts().toArray();
-                    for (int i=0; i<ArrayOfProducts.length/2; i++) {
-                        BranchProduct bp = ArrayOfProducts[rand.nextInt(ArrayOfProducts.length)];
+                    BranchProduct[] arrayOfProducts = b.getBranchProducts().toArray(new BranchProduct[0]);
+                    double sum = 0;
+                    for (int i = 0; i < arrayOfProducts.length / 2; i++) {
+                        BranchProduct bp = arrayOfProducts[rand.nextInt(arrayOfProducts.length)];
                         if (bp.getQuantity() > 10) {
                             int quantity = rand.nextInt(5) + 1;
-                            bp.setQuantity(bp.getQuantity()-quantity);
+                            bp.setQuantity(bp.getQuantity() - quantity);
                             sum += quantity * bp.getProduct().getPrice();
-                        }
-                        else {
+                        } else {
                             bp.setQuantity(30);
                         }
-                        BranchList.update(b);
                     }
-
+                    BranchList.update(b);
+                    System.out.println(b.getName() + " branch sold " + sum);
 
                 }
-
                 Thread.sleep(delay * 1000);
             }
-        }
-        catch(InterruptedException e) {
+        } catch (InterruptedException e) {
             System.out.println("Sales Interrupted");
         }
     }
